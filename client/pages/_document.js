f08feaf5c25a27cee8015ac2607cf9eb8a11b343
Samuel <timeuss.sam@gmail.com>
2023-01-31 00:37:00 +0100

    fix: camelCase typo

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -5,7 +5,7 @@ function MyDocument() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Anton&family=Cedarville+Cursive&family=Cousine:wght@400;700&display=swap"
           rel="stylesheet"
