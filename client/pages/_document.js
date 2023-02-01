48203e2b248181458ffe27d5cb390bef5fe68861
Samuel <timeuss.sam@gmail.com>
2023-02-01 12:51:16 +0100

    fix: fix crossorigin option

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -5,7 +5,7 @@ function MyDocument() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Anton&family=Cedarville+Cursive&family=Pontano+Sans&display=swap"
           rel="stylesheet"
